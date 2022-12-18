7c92f0057254e31fd0dcf3f11f2efddac7ce18c5
Caleb Korson <caleb@Calebs-MacBook-Air.local>
2022-12-18 09:49:31 -0500

    worked on basic structure of webpage

--- portfolio-app/src/components/pages/About.js
+++ portfolio-app/src/components/pages/About.js
@@ -3,14 +3,14 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        My name is Caleb Korson. I have a degree in chemical engineering from Michigan Tech University.
+        I worked for General Motors for 4 years as a Paint Process Engineer and Automation Specialist. 
+        I currently work for Consolidated Nuclear Securities as a Process Engineer, but I am looking expand 
+        my skill set and branch out from engineering.
+        With this in mind, I am currently taking the University of Vanderbilt Coding Bootcamp to earn a 
+        certificate as a full stack web developer
       </p>
     </div>
   );
--- portfolio-app/src/components/pages/Contact.js
+++ portfolio-app/src/components/pages/Contact.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'name') {
+      setName(inputValue)
+    } else if(inputType === 'email') {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1>Contact Me</h1>
+      
     </div>
   );
 }
