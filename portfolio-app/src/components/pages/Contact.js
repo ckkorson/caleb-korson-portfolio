b8d0fd04aef60ff533374bdbb895c2b636992019
Caleb Korson <caleb@Calebs-MacBook-Air.local>
2022-12-18 11:26:49 -0500

    added contact form and started styling header

--- portfolio-app/src/components/pages/Contact.js
+++ portfolio-app/src/components/pages/Contact.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Contact() {
+function Contact() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  // const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
+    
     if (inputType === 'name') {
       setName(inputValue)
     } else if(inputType === 'email') {
@@ -21,10 +19,44 @@ export default function Contact() {
       setMessage(inputValue);
     }
   };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert('Message sent!');
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
+
   return (
     <div>
       <h1>Contact Me</h1>
-      
+      <form>
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder='Name'
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder='Type your message here'
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
     </div>
   );
 }
+
+export default Contact;
